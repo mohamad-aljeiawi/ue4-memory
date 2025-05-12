6cd86732265ccc33706a44862f6a64b8f927bf48
mohamad <mobile.mohamad0789@gmail.com>
2025-05-12 21:50:52 +0300

    Update offsets in offset.h and refactor socket server initialization in main.cpp
    - Adjusted various offsets in offset.h for player and vehicle components to ensure accurate memory reading
    - Refactored socket server initialization in main.cpp to enable command handling and data transmission
    - Cleaned up commented-out code for improved readability and clarity

--- jni/include/utils/offset.h
+++ jni/include/utils/offset.h
@@ -6,8 +6,8 @@
 namespace Offset
 {
     //-- Base Offset G-Objects
-    uintptr_t g_name = 0xd877440;
-    uintptr_t g_world = 0xde24c70;
+    uintptr_t g_name = 0xdd879e0;
+    uintptr_t g_world = 0xe34b570;
     //-- Base Offset UWorld
     uintptr_t persistent_level = 0x30;
     uintptr_t u_level_to_a_actors = 0xA0;
@@ -30,7 +30,7 @@ namespace Offset
     //-- Class: StaticMeshComponent
     uintptr_t min_lob = 0x878; // StaticMesh* StaticMesh;
     //-- Class: UAEPlayerController
-    uintptr_t local_player_Key = 0x888; // uint32 PlayerKey;
+    uintptr_t local_player_Key = 0x898; // uint32 PlayerKey;
     uintptr_t local_team_id = 0x8b8;    // int TeamID;
     //-- Class: UAECharacter
     uintptr_t player_name = 0x8f0; // FString PlayerName;
@@ -38,7 +38,7 @@ namespace Offset
     uintptr_t team_id = 0x928;     // int TeamID;
     uintptr_t bis_ai = 0x9d9;      // bool bEnsure;
     //-- Class: STExtraBaseCharacter
-    uintptr_t bis_weapon_firing = 0x1688; // bool bIsWeaponFiring;
+    uintptr_t bis_weapon_firing = 0x16d0; // bool bIsWeaponFiring;
     //-- Class: STExtraCharacter
     uintptr_t health = 0xdb0;          // float Health;
     uintptr_t bis_dead = 0xdcc;        // bool bDead;
@@ -47,16 +47,16 @@ namespace Offset
     //-- Class: ItemDropMgrComponent
     uintptr_t grid_step = 0x1c0; // int GridStep;
     //-- Class: STExtraWeapon
-    uintptr_t weapon_entity_comp = 0x840; // WeaponEntity* WeaponEntityComp;
+    uintptr_t weapon_entity_comp = 0x770; // WeaponEntity* WeaponEntityComp;
     //-- Class: WeaponEntity
     uintptr_t weapon_id = 0x178; // int WeaponId;
     //-- Class: STExtraVehicleBase
-    uintptr_t vehicle_common = 0xaf8; // VehicleCommonComponent* VehicleCommon;
+    uintptr_t vehicle_common = 0xb30; // VehicleCommonComponent* VehicleCommon;
     //-- Class: VehicleCommonComponent
-    uintptr_t hp_max = 0x2c0;   // float HPMax;
-    uintptr_t hp = 0x2c4;       // float HP;
-    uintptr_t fuel_max = 0x380; // float FuelMax;
-    uintptr_t fuel = 0x384;     // float Fuel;
+    uintptr_t hp_max = 0x2d8;   // float HPMax;
+    uintptr_t hp = 0x2dc;       // float HP;
+    uintptr_t fuel_max = 0x398; // float FuelMax;
+    uintptr_t fuel = 0x39c;     // float Fuel;
 
 	// old dont ues-----------------------------------------------
     //--Class: STExtraShootWeapon
--- jni/src/main.cpp
+++ jni/src/main.cpp
@@ -29,19 +29,19 @@ bool init()
 {
     LOGI("Initializing...");
 
-    // socket_server = std::make_unique<SocketServer>("/data/local/tmp/game_data_socket");
-    // socket_server->set_command_callback(handle_command);
-    // if (!socket_server->start())
-    // {
-    //     LOGE("Failed to start socket server");
-    //     return false;
-    // }
+    socket_server = std::make_unique<SocketServer>("/data/local/tmp/game_data_socket");
+    socket_server->set_command_callback(handle_command);
+    if (!socket_server->start())
+    {
+        LOGE("Failed to start socket server");
+        return false;
+    }
 
-    // fps_controller.set_send_data_callback([](const std::string &msg)
-    //                                       {
-    //    if (socket_server) {
-    //     socket_server->send_data(msg);
-    //     } });
+    fps_controller.set_send_data_callback([](const std::string &msg)
+                                          {
+       if (socket_server) {
+        socket_server->send_data(msg);
+        } });
     fps_controller.set_target_fps(40);
 
     is_running = true;
@@ -82,8 +82,8 @@ int main(int argc, char *argv[])
         fps_controller.start_frame();
 
         uintptr_t u_world = Memory::Read<uintptr_t>(Memory::Read<uintptr_t>(g_world + 0x58, target_pid) + 0x78, target_pid);
-        // uintptr_t player_controller = Memory::Read<uintptr_t>(Memory::Read<uintptr_t>(Memory::Read<uintptr_t>(u_world + 0x38, target_pid) + 0x78, target_pid) + 0x30, target_pid);
         // uintptr_t u_world = Memory::Read<uintptr_t>(g_world + 0x20, target_pid);
+        uintptr_t player_controller = Memory::Read<uintptr_t>(Memory::Read<uintptr_t>(Memory::Read<uintptr_t>(u_world + 0x38, target_pid) + 0x78, target_pid) + 0x30, target_pid);
         uintptr_t u_level = Memory::Read<uintptr_t>(u_world + Offset::persistent_level, target_pid);
         uintptr_t entity_list = Ue4::get_actors_array(u_level, Offset::u_level_to_a_actors, 0x448, target_pid);
         uintptr_t u_level_to_a_actors = Memory::Read<uintptr_t>(entity_list, target_pid);
@@ -95,8 +95,17 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        // uintptr_t player_camera_manager = Memory::Read<uintptr_t>(player_controller + Offset::player_camera_manager, target_pid);
+        // if (player_camera_manager <= 0)
+        //     continue;
+        // Structs::CameraCacheEntry camera_cache = Memory::Read<Structs::CameraCacheEntry>(player_camera_manager + Offset::camera_cache, target_pid);
+        // Structs::MinimalViewInfo minimal_view_info = camera_cache.POV;
+
         for (int i = 0; i < u_level_to_a_actors_count; i++)
         {
+
+            // printf("MinimalViewInfo: %0.1f, %0.1f, %0.1f, %0.1f\n", minimal_view_info.Location.X, minimal_view_info.Location.Y, minimal_view_info.Location.Z, minimal_view_info.FOV);
+
             uintptr_t actor = Memory::Read<uintptr_t>(u_level_to_a_actors + i * sizeof(uintptr_t), target_pid);
             if (!actor)
             {
@@ -111,8 +120,69 @@ int main(int argc, char *argv[])
 
             printf("Class Name: %s\n", class_name.c_str());
 
-            // if (Utils::is_contains(class_name, "BP_STPlayerStart") || Utils::is_contains(class_name, "BP_PlayerPawn") || Utils::is_contains(class_name, "BP_PlayerState"))
-            // {
+            if (Utils::is_contains(class_name, "BP_STPlayerStart") || Utils::is_contains(class_name, "BP_PlayerPawn") || Utils::is_contains(class_name, "BP_PlayerState"))
+            {
+
+                uintptr_t actor_root_component = Memory::Read<uintptr_t>(actor + Offset::root_component, target_pid);
+                if (actor_root_component == 0)
+                {
+                    continue;
+                }
+
+                Structs::FVector actor_location = Memory::Read<Structs::FVector>(actor_root_component + Offset::relative_location, target_pid);
+                Structs::FRotator actor_rotation = Memory::Read<Structs::FRotator>(actor_root_component + Offset::relative_rotation, target_pid);
+
+                // printf("Class Name: %s\n", class_name.c_str());
+                // printf("Location: %0.1f, %0.1f, %0.1f\n", actor_location.X, actor_location.Y, actor_location.Z);
+                // printf("Rotation: %0.1f, %0.1f, %0.1f\n", actor_rotation.Pitch, actor_rotation.Yaw, actor_rotation.Roll);
+
+                // printf("Class Name: %s\n", class_name.c_str());
+                socket_server->send_data(class_name + "\n");
+            }
+
+            // std::string class_name = Ue4::get_class_name(actor, g_names, target_pid);
+            // printf("Actor %d: %s\n", i, class_name.c_str());
+
+            // int actor_id = Memory::Read<int>(actor + 0x18, target_pid);
+            // if (actor_id <= 0)
+            //     continue;
+
+            // uintptr_t g_names = Memory::Read<uintptr_t>(lib_base + Offset::g_name, target_pid);
+            // uintptr_t name_entry = Memory::Read<uintptr_t>(g_names + (actor_id >> 0x10) * sizeof(uintptr_t), target_pid);
+            // if (name_entry == 0)
+            //     continue;
+
+            // name_entry = Memory::Read<uintptr_t>(name_entry + (actor_id & 0xFFFF) * sizeof(uintptr_t), target_pid);
+            // if (name_entry == 0)
+            //     continue;
+
+            // int name_length = Memory::Read<uint16_t>(name_entry + 0x10, target_pid);
+            // if (name_length <= 0 || name_length > 100)
+            //     continue;
+
+            // std::string actor_type = Memory::ReadString(name_entry + 0x14, target_pid);
+
+            // printf("Actor %d: %s\n", i, actor_type.c_str());
+
+            //     std::string player_name = Memory::ReadString(actor + Offset::player_name, target_pid);
+            //     if (player_name.empty())
+            //     {
+            //         continue;
+            //     }
+
+            //     int team_id = Memory::Read<int>(actor + Offset::team_id, target_pid);
+            //     if (team_id <= 0 || team_id > 100)
+            //     {
+            //         continue;
+            //     }
+
+            //     float health = Memory::Read<float>(actor + Offset::health, target_pid);
+            //     if (health <= 0)
+            //     {
+            //         continue;
+            //     }
+
+            //     bool is_bot = Memory::Read<bool>(actor + Offset::bis_ai, target_pid);
 
             //     uintptr_t actor_root_component = Memory::Read<uintptr_t>(actor + Offset::root_component, target_pid);
             //     if (actor_root_component == 0)
@@ -120,12 +190,16 @@ int main(int argc, char *argv[])
             //         continue;
             //     }
 
-            //     Structs::FVector actor_location = Memory::Read<Structs::FVector>(actor_root_component + Offset::relative_location, target_pid);
-            //     Structs::FRotator actor_rotation = Memory::Read<Structs::FRotator>(actor_root_component + Offset::relative_rotation, target_pid);
+            //     Ue4::FVector actor_location = Memory::Read<Ue4::FVector>(actor_root_component + Offset::relative_location, target_pid);
+            //     Ue4::FRotator actor_rotation = Memory::Read<Ue4::FRotator>(actor_root_component + Offset::relative_rotation, target_pid);
 
-            //     printf("Class Name: %s\n", class_name.c_str());
-            //     // socket_server->send_data(class_name + "\n");
-            // }
+            //     printf("Player %d [%s] Team=%d Bot=%s Health=%.0f Location=(%0.1f, %0.1f, %0.1f)\n",
+            //            i,
+            //            player_name.c_str(),
+            //            team_id,
+            //            is_bot ? "Yes" : "No",
+            //            health,
+            //            actor_location.X, actor_location.Y, actor_location.Z);
         }
 
         std::string command;
